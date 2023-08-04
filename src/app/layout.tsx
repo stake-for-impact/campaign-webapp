5ad687ce3de1e5dae0528a8ff19758ad9219197d
Oleksiy Novykov <services@novykov.com>
2023-08-04 20:28:12 +0200

    Added Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import React from 'react'
 import './global.css'
 import '../input.css'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: 'wagmi',
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   )

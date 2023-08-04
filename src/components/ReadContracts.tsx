8c12e47a6af0ff10e9014de1390453b494899f3e
Oleksiy Novykov <services@novykov.com>
2023-08-04 20:45:45 +0200

    Bug fix

--- src/components/ReadContracts.tsx
+++ src/components/ReadContracts.tsx
@@ -2,25 +2,11 @@
 
 import { useContractReads } from 'wagmi'
 
-import { wagmiContractConfig } from './contracts'
 import { stringify } from '../utils/stringify'
 
 export function ReadContracts() {
   const { data, isSuccess, isLoading } = useContractReads({
     contracts: [
-      {
-        ...wagmiContractConfig,
-        functionName: 'balanceOf',
-        args: ['0xa5cc3c03994DB5b0d9A5eEdD10CabaB0813678AC'],
-      },
-      {
-        ...wagmiContractConfig,
-        functionName: 'name',
-      },
-      {
-        ...wagmiContractConfig,
-        functionName: 'totalSupply',
-      },
     ],
   })
 
